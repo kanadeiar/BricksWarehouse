1fb307d5e4eccde8f8cbf47512bdf6add62f0372
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-11-29 15:36:26 +0400

    Базовая инфраструктура веб приложения

---
+++ BricksWarehouse/BricksWarehouse/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BricksWarehouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error(string id)
+        {
+            switch (id)
+            {
+                default: return Content($"Status --- {id}");
+                case "404": return View("Error404");
+            }
+        }
+    }
+}
--- BricksWarehouse/BricksWarehouse/Program.cs
+++ BricksWarehouse/BricksWarehouse/Program.cs
@@ -1,6 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureServices(services =>
+{
+    services.AddControllersWithViews().AddRazorRuntimeCompilation();
+});
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStatusCodePagesWithRedirects("~/home/error/{0}");
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+app.MapBlazorHub();
 
 app.Run();
