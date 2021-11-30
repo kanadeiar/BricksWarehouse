698345965e5ea4f540922f355c1dc6af7c2b4abe
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-11-30 10:33:59 +0400

    Сущности для данных

---
+++ BricksWarehouse/BricksWarehouse.Domain/Models/Base/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace BricksWarehouse.Domain.Models.Base
+{
+    public abstract class Entity : INotifyPropertyChanged
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ BricksWarehouse/BricksWarehouse.Domain/Models/ProductType.cs
@@ -0,0 +1,61 @@
+﻿using BricksWarehouse.Domain.Models.Base;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BricksWarehouse.Domain.Models
+{
+    /// <summary> Вид товаров </summary>
+    public class ProductType : Entity
+    {
+        private string _Name;
+        /// <summary> Название </summary>
+        [Required(ErrorMessage = "Название вида товаров обязательно")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Название вида товаров должно быть длинной от 3 до 200 символов")]
+        public string Name
+        {
+            get => _Name;
+            set => Set(ref _Name, value);
+        }
+        private int _Order;
+        /// <summary> Сортировка </summary>
+        public int Order
+        {
+            get => _Order;
+            set => Set(ref _Order, value);
+        }
+        private int _Units;
+        /// <summary> Количество едениц в пачке </summary>
+        [Range(0, int.MaxValue, ErrorMessage = "Количество едениц в пачке товаров должно быть положительным числом")]
+        public int Units
+        {
+            get => _Units;
+            set => Set(ref _Units, value);
+        }
+        private double _Volume;
+        /// <summary> Объем, занимаемый одной пачкой </summary>
+        [Range(0.0, double.MaxValue, ErrorMessage = "Объем одной пачки должен быть положительным числом")]
+        public double Volume
+        {
+            get => _Volume;
+            set => Set(ref _Volume, value);
+        }
+        private double _Weight;
+        /// <summary> Вес одной пачки </summary>
+        [Range(0.0, double.MaxValue, ErrorMessage = "Вес одной пачки должен быть положительным числом")]
+        public double Weight
+        {
+            get => _Weight;
+            set => Set(ref _Weight, value);
+        }
+        private bool _IsDelete;
+        /// <summary> Метка удаления вида товаров </summary>
+        public bool IsDelete
+        {
+            get => _IsDelete;
+            set => Set(ref _IsDelete, value);
+        }
+
+        /// <summary> Места размещения этого вида товаров </summary>
+        public ICollection<Place> Places { get; set; }
+    }
+}
