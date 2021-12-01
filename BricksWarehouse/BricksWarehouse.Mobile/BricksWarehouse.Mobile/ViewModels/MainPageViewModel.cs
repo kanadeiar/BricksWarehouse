04e6eb58966aeba24789a399a75f543dbf028877
kanadeiar <kanadeiar@gmail.com>
2021-12-01 23:03:42 +0400

    Вывод информации на обзоре склада

--- BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/ViewModels/MainPageViewModel.cs
+++ BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,13 @@
-﻿using BricksWarehouse.Mobile.ViewModels.Base;
+﻿using BricksWarehouse.Domain.Models;
+using BricksWarehouse.Mobile.Services;
+using BricksWarehouse.Mobile.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace BricksWarehouse.Mobile.ViewModels
 {
@@ -9,13 +15,25 @@ namespace BricksWarehouse.Mobile.ViewModels
     {
         #region Данные
 
+        private readonly ProductTypeClient _productTypeClient;
+
         #endregion
 
         #region Свойства
 
+        public ObservableCollection<ProductTypeView> ProductTypes { get; set; } = new ObservableCollection<ProductTypeView>();
 
+        private bool _refreshingProductTypes;
+        /// <summary> Обновление данных </summary>
+        public bool RefreshingProductTypes
+        {
+            get => _refreshingProductTypes;
+            set => Set(ref _refreshingProductTypes, value);
+        }
 
         private string _Title = "Склад кирпичей";
+
+
         /// <summary> Название приложения </summary>
         public string Title
         {
@@ -25,21 +43,55 @@ namespace BricksWarehouse.Mobile.ViewModels
 
         #endregion
 
-        public MainPageViewModel()
+        public MainPageViewModel(ProductTypeClient productTypeClient)
         {
+            _productTypeClient = productTypeClient;
 
+            Task.Run(async () =>
+            {
+                await UpdateProductTypes();
+                RefreshingProductTypes = false;
+            });
         }
 
         #region Команды
 
 
 
+        private ICommand _UpdateProductTypesCommand;
+        /// <summary> Обновить </summary>
+        public ICommand UpdateProductTypesCommand => _UpdateProductTypesCommand ??= new Command(OnUpdateProductTypesCommandExecuted);
+        private async void OnUpdateProductTypesCommandExecuted(object p)
+        {
+            RefreshingProductTypes = true;
+            await UpdateProductTypes();
+            RefreshingProductTypes = false;
+        }
+
         #endregion
 
         #region Вспомогательное
 
-
+        public async Task UpdateProductTypes()
+        {
+            var productTypes = await _productTypeClient.GetAll();
+            ProductTypes.Clear();
+            foreach (var productType in productTypes)
+                ProductTypes.Add( new ProductTypeView
+                {
+                    Name = productType.Name,
+                    PlacesCount = productType.Places.Count,
+                    CountUnits = productType.Places.Count * productType.Units,
+                });
+        }
 
         #endregion
+    }
+
+    public class ProductTypeView
+    {
+        public string Name { get; set; }
+        public int PlacesCount { get; set; }
+        public int CountUnits { get; set; }
     }
 }
