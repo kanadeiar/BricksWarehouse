a3344950ada1e95842dfaf225fd2219aa7e76719
kanadeiar <kanadeiar@gmail.com>
2021-12-01 20:43:47 +0400

    Еще основа приложения

--- BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/App.xaml.cs
+++ BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/App.xaml.cs
@@ -3,6 +3,11 @@ using Xamarin.Forms;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net.Http;
 using BricksWarehouse.Mobile.ViewModels;
+using BricksWarehouse.Domain.Models;
+using BricksWarehouse.Domain.Dto;
+using BricksWarehouse.Domain.Interfaces;
+using BricksWarehouse.Domain.Mappers;
+using BricksWarehouse.Mobile.Services;
 
 namespace BricksWarehouse.Mobile
 {
@@ -19,6 +24,14 @@ namespace BricksWarehouse.Mobile
         private static void InitServices(IServiceCollection services)
         {
             services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://brickswarehouse.azurewebsites.net") });
+
+            services.AddScoped<ProductTypeClient>();
+            services.AddScoped<PlaceClient>();
+
+            services.AddScoped<IMapper<ProductType, ProductTypeDto>, DtoMapperService>();
+            services.AddScoped<IMapper<ProductTypeDto, ProductType>, DtoMapperService>();
+            services.AddScoped<IMapper<Place, PlaceDto>, DtoMapperService>();
+            services.AddScoped<IMapper<PlaceDto, Place>, DtoMapperService>();
 
             services.AddSingleton<MainPageViewModel>();
 
--- BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/Views/MainPage.xaml.cs
+++ BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/Views/MainPage.xaml.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BricksWarehouse.Mobile.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 using Xamarin.Forms;
 
 namespace BricksWarehouse.Mobile.Views
 {
     public partial class MainPage : ContentPage
     {
+        public MainPageViewModel ViewModel { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            ViewModel = App.Services.GetRequiredService<MainPageViewModel>();
         }
     }
 }
