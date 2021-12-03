3ef3bc4f02ea78ea174e188988d57d1e878c7483
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-12-03 09:34:59 +0400

    Добавлена геолокация

---
+++ BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/Views/GeolocationPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BricksWarehouse.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GeolocationPage : ContentPage
+    {
+        public GeolocationPage()
+        {
+            InitializeComponent();
+        }
+
+        private async Task btnLocation_ClickedAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    LabelLatitude.Text = "Широта: " + location.Latitude.ToString();
+                    LabelLongitude.Text = "Долгота: " + location.Longitude.ToString();
+                    LabelCoordinates.Text += $"Широта: {location.Latitude} Долгота: {location.Longitude}\n";
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Ошибка", fnsEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Ошибка", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ошибка", ex.Message, "OK");
+            }
+        }
+    }
+}
