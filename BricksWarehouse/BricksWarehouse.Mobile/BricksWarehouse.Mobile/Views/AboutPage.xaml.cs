b63cc54823581088e7aa1f647e24727fe79d9a0e
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-12-03 10:33:40 +0400

    Ссылка на сайт

--- BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/Views/AboutPage.xaml.cs
+++ BricksWarehouse/BricksWarehouse.Mobile/BricksWarehouse.Mobile/Views/AboutPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,11 @@ namespace BricksWarehouse.Mobile.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        private async void ButtonToSite_Clicked(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync(new Uri("https://brickswarehouse.azurewebsites.net/"), BrowserLaunchMode.SystemPreferred);
         }
     }
 }
