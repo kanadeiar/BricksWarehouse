6a6045e3c2c4508ac731eeee90039e264bff3050
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2022-01-10 11:26:01 +0400

    Tests

---
+++ Tests/BricksWarehouse.Tests/Services/CountsInfoServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BricksWarehouse.Interfaces.Services;
+using BricksWarehouse.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BricksWarehouse.Tests.Services
+{
+    [TestClass]
+    public class CountsInfoServiceTests
+    {
+        [TestMethod]
+        public void GetCounts_CallZero_ShouldCorrectType()
+        {
+            var productTypeStub = Mock
+                .Of<IProductTypeData>();
+            var service = new CountsInfoService(productTypeStub);
+
+            var actual = service.GetCounts().Result;
+
+            Assert
+                .IsInstanceOfType(actual, typeof(IEnumerable<CountsInfoWebModel>));
+            Assert
+                .AreEqual(0, actual.Count());
+        }
+    }
+}
